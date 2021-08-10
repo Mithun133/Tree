1f1f404704827114d69ab101b83c5107f76f3113
unknown <mithunchandradas133@gmail.com>
2021-08-10 18:51:42 +0100

    Tree Implementation done

--- tree.py
+++ tree.py
@@ -1,4 +1,14 @@
-# tree implementation
+# Implementing a Tree
+"""
+            4
+          /   \
+        5       6
+       / \       \
+      1   7       9
+         / \     / \
+        3  10   2   8
+    Left | Node | Right
+"""
 
 class Node:
     def __init__(self, data):
@@ -6,39 +16,38 @@ class Node:
         self.left = None
         self.right = None
 
-    def add_left(self,node):
+    def add_left(self, node):
         self.left = node
-
-    def add_right(self,node):
+    
+    def add_right(self, node):
         self.right = node
 
 def create_tree():
-    two = Node(2)
-    seven = Node(7)
-    nine = Node(9)
-    two.add_left(seven)
-    two.add_right(nine)
-
+    four = Node(4)
+    five = Node(5)
+    six = Node(6)
+    four.add_left(five)
+    four.add_right(six)
 
     one = Node(1)
-    six = Node(6)
-    seven.add_left(one)
-    seven.add_right(six)
-
-    five = Node(5)
-    ten = Node(10)
-    six.add_left(five)
-    six.add_right(ten)
-
-    eight = Node(8)
-    nine.add_right(eight)
+    seven = Node(7)
+    five.add_left(one)
+    five.add_right(seven)
 
     three = Node(3)
-    four = Node(4)
-    eight.add_left(three)
-    eight.add_right(four)
+    ten = Node(10)
+    seven.add_left(three)
+    seven.add_right(ten)
 
-    return two
+    nine = Node(9)
+    six.add_right(nine)
+
+    two = Node(2)
+    eight = Node(8)
+    nine.add_left(two)
+    nine.add_right(eight)
+
+    return four
 
 def pre_order(node):
     print(node.data, end=' ')
@@ -46,10 +55,29 @@ def pre_order(node):
         pre_order(node.left)
     if node.right:
         pre_order(node.right)
+    
+def post_order(node):
+    if node.left:
+        post_order(node.left)
+    if node.right:
+        post_order(node.right)
+    print(node.data, end=' ')
+
+def in_order(node):
+    if node.left:
+        in_order(node.left)
+    print(node.data, end=' ')
+    if node.right:
+        in_order(node.right)
 
 if __name__ == '__main__':
     tree = create_tree()
     print(tree.data)
+    print('\n*Pre_order')
     pre_order(tree)
 
-        
+    print('\n\n*post_order')
+    post_order(tree)
+
+    print('\n\n*In_order')
+    in_order(tree)
